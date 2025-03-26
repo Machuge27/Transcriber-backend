1980b0cebe2aa9ff850b5eb8ee8295616a39eb38
Machuge27 <mutaihillary3912@gmail.com>
2025-03-26 22:34:16 +0300

    Cors error fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://transcriber-henna.vercel.app/dashboard",
+    "https://transcriber-henna.vercel.app",
 ]
 
 # Application definition
